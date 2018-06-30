52e7fb193550bceb59b43f58591207be9c642f6d
bartoszlog <bartosz.logwiniuk@gmail.com>
2018-06-30 15:03:12 +0200

    training vuejs

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid @quoteDeleted="deleteQuote" :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue'
+    import NewQuote from './components/NewQuote.vue'
+    export default {
+        data() {
+            return {
+                quotes: [
+                    "Simple quote"
+                ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        },
+        methods: {
+            newQuote(quote) {
+                this.quotes.push(quote)
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1)
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Quote!</h3>
+        <button class="btn btn-primary" @click.prevent="addQuote">Add Quote!</button>
+        <div>
+            <textarea v-model="quote" cols="30" rows="10"></textarea>
+        </div>        
+    </div>
+</template>
+
+
+<script>
+    export default {
+        data() {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            addQuote() {
+                this.$emit('quoteAdded', this.quote)
+                this.quote = ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
